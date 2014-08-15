93aad27b0070dda560d2a1693f6981b02712e5b1
dspp779 <dspp779@gmail.com>
2014-08-15 17:02:36 +0800

    Search function
    search for project, multiple keywords allowed by separating with blank,
    but wiith bug that multiple bland leads to unexpected result.

--- Mirle.iMServer.Model/Utility/ModelUtil.cs
+++ Mirle.iMServer.Model/Utility/ModelUtil.cs
@@ -13,33 +13,65 @@ namespace Mirle.iMServer.Model
     {
         public static List<ProjectData> getProjectList()
         {
-            try
+            List<ProjectData> pList = new List<ProjectData>();
+            MySqlDbInterface db = new MySqlDbInterface();
+            using (DbConnection conn = db.getConnection())
             {
-                List<ProjectData> pList = new List<ProjectData>();
-                MySqlDbInterface db = new MySqlDbInterface();
-                using (DbConnection conn = db.getConnection())
-                {
-                    conn.Open();
-                    DbCommand cmd = new MySqlCommand("SELECT * FROM Project");
-                    cmd.Connection = conn;
-                    using (MySqlDataReader reader = cmd.ExecuteReader() as MySqlDataReader)
-                    {
-                        while (reader.Read())
-                        {
-                            // get column values
-                            pList.Add(
-                                new ProjectData(reader.GetInt64("id"), reader.GetString("name"),
-                                    reader.GetString("alias"), reader.GetString("addr"),
-                                    reader.GetDouble("lat"), reader.GetDouble("lng"))
-                            );
-                        }
-                    }
-                }
-                return pList;
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM Project");
+                cmd.Connection = conn as MySqlConnection;
+                getProjectList(cmd, pList);
             }
-            catch (Exception)
+            return pList;
+        }
+
+        public static List<ProjectData> getProjectList(string keyword)
+        {
+            List<ProjectData> pList = new List<ProjectData>();
+            MySqlDbInterface db = new MySqlDbInterface();
+            using (DbConnection conn = db.getConnection())
             {
-                return new List<ProjectData>();
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM Project WHERE name LIKE @keyword");
+                cmd.Parameters.AddWithValue("@keyword", '%' + keyword + '%');
+                cmd.Connection = conn as MySqlConnection;
+                getProjectList(cmd, pList);
+            }
+            return pList;
+        }
+
+        public static List<ProjectData> getProjectList(string[] keywords)
+        {
+            List<ProjectData> pList = new List<ProjectData>();
+            MySqlDbInterface db = new MySqlDbInterface();
+            using (DbConnection conn = db.getConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM Project WHERE name LIKE @keyword");
+                cmd.Connection = conn as MySqlConnection;
+                foreach (string keyword in keywords)
+                {
+                    cmd.Parameters.Clear();
+                    cmd.Parameters.AddWithValue("@keyword", '%' + keyword + '%');
+                    getProjectList(cmd, pList);
+                }
+            }
+            return pList;
+        }
+
+        public static void getProjectList(MySqlCommand cmd, List<ProjectData> pList)
+        {
+            using (MySqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    // get column values
+                    pList.Add(
+                        new ProjectData(reader.GetInt64("id"), reader.GetString("name"),
+                            reader.GetString("alias"), reader.GetString("addr"),
+                            reader.GetDouble("lat"), reader.GetDouble("lng"))
+                    );
+                }
             }
         }
     }
--- Mirle_GPLC/MainWindow.xaml.cs
+++ Mirle_GPLC/MainWindow.xaml.cs
@@ -42,40 +42,6 @@ namespace Mirle_GPLC
             //MainMap.Manager.SecondaryCache = ch;
         }
 
-        private void loadProjectData()
-        {
-            // 載入所有專案
-            this.pList = ModelUtil.getProjectList();
-            // 更新專案列表
-            // 清空專案列表
-            projectListView.Items.Clear();
-            // 加入專案列表
-            foreach (ProjectData p in pList)
-            {
-                projectListView.Items.Add(p);
-                addPMarker(p);
-            }
-        }
-
-        // add marker to overlay delegate
-        delegate void addMarkerHandler(GMapMarker marker);
-        private void addMarker(GMapMarker marker)
-        {
-            // add to overlay
-            gMap.Markers.Add(marker);
-        }
-        private void addPMarker(ProjectData p)
-        {
-            addPMarker(new PointLatLng(p.lat, p.lng), p);
-        }
-        private void addPMarker(PointLatLng latlng, ProjectData p)
-        {
-            GMapMarker marker = new GMapMarker(latlng);
-            marker.Shape = new ProjectMarker(gMap, marker, p);
-            addMarker(marker);
-        }
-
-        #region -- Event Handlers --
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             // 初始化地圖元件
@@ -93,12 +59,65 @@ namespace Mirle_GPLC
             gMap.DragButton = MouseButton.Left;
             // 設定初始位置
             gMap.Position = new PointLatLng(23.8, 121);
-            // 設定滑鼠滾輪忽略標記，以縮放地圖
-            //gMap.IgnoreMarkerOnMouseWheel = true;
 
+            //textBox_searchProject.button
+
+            // 初始化當前Marker為空值
+            currentMarker = null;
             // 載入專案資料
-            loadProjectData();
+            loadProjectData("");
+            refreshProjectData();
         }
+
+        private void loadProjectData(string keyword)
+        {
+            string[] strs = keyword.Split(' ');
+            // 載入所有專案
+            this.pList = ModelUtil.getProjectList(strs);
+        }
+
+        private void refreshProjectData()
+        {
+            // 更新專案列表
+            // 清空專案列表
+            projectListView.Items.Clear();
+            // 加入專案列表
+            foreach (ProjectData p in pList)
+            {
+                GMapMarker pm = newProjectMarker(p);
+                projectListView.Items.Add(p);
+                newProjectMarker(p);
+            }
+        }
+
+        private void searchProject(string keyword)
+        {
+            loadProjectData(keyword);
+            refreshProjectData();
+        }
+
+        #region -- marker functions --
+        private GMapMarker newProjectMarker(ProjectData p)
+        {
+            return newProjectMarker(new PointLatLng(p.lat, p.lng), p);
+        }
+        private GMapMarker newProjectMarker(PointLatLng latlng, ProjectData p)
+        {
+            GMapMarker marker = new GMapMarker(latlng);
+            marker.Shape = new ProjectMarker(gMap, marker, p);
+            addMarker(marker);
+            return marker;
+        }
+        // add marker to overlay delegate
+        delegate void addMarkerHandler(GMapMarker marker);
+        private void addMarker(GMapMarker marker)
+        {
+            // add to overlay
+            gMap.Markers.Add(marker);
+        }
+        #endregion
+
+        #region -- Event Handlers --
 
         private void comboBox_maptype_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -115,6 +134,12 @@ namespace Mirle_GPLC
                     break;
             }
         }
+
+        private void textBox_searchProject_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            searchProject(textBox_searchProject.Text);
+        }
+
         #endregion
 
     }
